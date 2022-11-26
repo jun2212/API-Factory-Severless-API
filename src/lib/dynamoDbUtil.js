037c1069a772af2c1613d6cf191ecdcdf9d38cfb
jun2212 <hsj890509@gmail.com>
2022-11-26 19:49:04 +0900

    feat: 사용자가 다이나오db를 사용할 수 있는 라이브러리 구현

--- src/lib/dynamoDbUtil.js
+++ src/lib/dynamoDbUtil.js
@@ -2,16 +2,53 @@ const AWS = require("aws-sdk");
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-const tableName = "user_function";
-
 const findUserFunction = async (functionKey) => {
   const params = {
-    TableName: tableName,
+    TableName: "user_function",
     Key: { function_key: functionKey },
   };
 
   return await dynamoDB.get(params).promise();
 };
-const setDynamoDbCustomLibrary = () => {};
+
+const setDynamoDbCustomLibrary = (user_id) => {
+  const getDbData = async (divisionKey) => {
+    const params = {
+      TableName: "API-FACTORY-DB",
+      Key: { user_id: user_id, division_key: divisionKey },
+    };
+
+    const { Item } = await dynamoDB.get(params).promise();
+
+    delete Item["user_id"];
+    delete Item["division_key"];
+
+    return Item;
+  };
+
+  const setDbData = async (divisionKey, data) => {
+    const Item = {
+      user_id: user_id,
+      division_key: divisionKey,
+    };
+
+    for (const key in data) {
+      if (key === "user_id" || key === "division_key") {
+        throw new Error("invalid column name");
+      }
+
+      Item[key] = data[key];
+    }
+
+    const params = {
+      TableName: "API-FACTORY-DB",
+      Item: Item,
+    };
+
+    return await dynamoDB.put(params).promise();
+  };
+
+  return { getDbData, setDbData };
+};
 
 module.exports = { findUserFunction, setDynamoDbCustomLibrary };
